de04f8f5a4d1b5b2ad23d0234a7681341aa0e923
Umar Farooq <hme465436@gmail.com>
2026-05-13 12:46:54 +0000

    remove empty space (_for element div_)

--- src/components/NativeAd.tsx
+++ src/components/NativeAd.tsx
@@ -8,8 +8,8 @@ interface NativeAdProps {
 
 export function NativeAd({ className }: NativeAdProps) {
   return (
-    <div className={`w-full flex justify-center py-8 ${className || ''}`}>
-      <div className="w-full max-w-4xl overflow-hidden min-h-[100px] flex flex-col items-center">
+    <div className={`w-full flex justify-center ${className || ''}`}>
+      <div className="w-full max-w-4xl overflow-hidden flex flex-col items-center">
         {/* Ad Container */}
         <div id="container-87432722d80db14701cb21e3bd1c4184" className="w-full"></div>
         {/* Ad Script */}
