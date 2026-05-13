ac24a4669fef4be3515ed7065397e505ff9fc97a
Umar Farooq <hme465436@gmail.com>
2026-05-13 13:25:46 +0000

    Add Google Search Console verification meta tag in the website.
    Add thi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ export const metadata: Metadata = {
   title: 'WhatsQuality - WhatsApp Full DP Maker | HD Profile Picture No Crop',
   description: 'Create a full size WhatsApp DP without cropping. WhatsQuality is the best HD WhatsApp profile picture maker to resize and fit photos perfectly. 100% Free.',
   keywords: 'WhatsApp DP Maker, WhatsApp Full DP, HD WhatsApp DP, Full Size WhatsApp DP without cropping, WhatsApp Profile Picture Maker, WhatsApp DP without cropping, Best WhatsApp DP maker',
+  verification: {
+    google: '3WwAf-XMxfYwO7_meXqlBvIjQBUhYaOCxkhCwJrtFJU',
+  },
 };
 
 export default function RootLayout({
